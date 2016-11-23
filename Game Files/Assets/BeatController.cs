4f55df617035472cc025f798fb441cfed18f7287
Callam Mutton <S171919@ucs.ac.uk>
2016-11-23 11:07:18 +0000

    Add files via upload
    Added Unity game files.

---
+++ Game Files/Assets/BeatController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Beat {
+	public float offset;
+	public float interval;
+
+	public Beat (float offset, float interval) {
+		this.offset = offset;
+		this.interval = Mathf.Abs(interval);
+	}
+
+	public float position (float time) {
+		return (time + offset) % interval;
+	}
+}
+
+public class BeatController : MonoBehaviour {
+
+	public GameObject beatPrefab; //Set the sprite object
+	public Transform beatTransform; //Set gameobject position
+
+	private GameObject beatObject; //
+	private Beat beat; //
+
+	// Use this for initialization
+	void Start () {
+		beat = new Beat (0, 2); //Beat is equal to 
+		beatObject = Instantiate(beatPrefab); //Create new gameobject
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var m = beatObject.GetComponent(typeof(Transform)) as Transform;
+		var p = beat.position (Time.time);
+		m.position = new Vector2 (p, 0);
+	}
+}
---
+++ Game Files/Assets/Notes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Notes : MonoBehaviour {
+
+    public Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+	
+        if(gameObject.name == "notes_6") //Check the gameobject is note_6
+        {
+
+            GetComponent<BoxCollider2D>(); //Add the 2D collider to the object
+            rb2D.velocity = new Vector2(-100, 0); //Get the new velocity for the object
+
+        }
+
+        if (gameObject.name == "notes_7")
+        {
+
+            GetComponent<BoxCollider2D>();
+            rb2D.velocity = new Vector2(-100, 0);
+
+        }
+
+        if (gameObject.name == "notes_8")
+        {
+
+            GetComponent<BoxCollider2D>();
+            rb2D.velocity = new Vector2(100, 0);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
